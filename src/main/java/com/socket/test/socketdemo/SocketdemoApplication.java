30e147301c9a80a590fd868b73af744c9180bba1
zy <jinggai987@163.com>
2020-03-08 16:30:03 +0800

    配置

---
+++ src/main/java/com/socket/test/socketdemo/SocketdemoApplication.java
@@ -0,0 +1,13 @@
+package com.socket.test.socketdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SocketdemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SocketdemoApplication.class, args);
+	}
+
+}
