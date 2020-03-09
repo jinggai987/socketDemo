bf685fd7ac7c934c6ad686cc5e4246755b67da93
jinggai <34874320+jinggai987@users.noreply.github.com>
2020-03-09 21:43:30 +0800

    区分tcp/udp

--- src/test/java/com/socket/test/socketdemo/tcp/SocketClientTest.java
+++ src/test/java/com/socket/test/socketdemo/tcp/SocketClientTest.java
@@ -1,4 +1,4 @@
-package com.socket.test.socketdemo;
+package com.socket.test.socketdemo.tcp;
 
 import java.io.*;
 import java.net.Socket;
@@ -9,25 +9,25 @@ import java.net.UnknownHostException;
  */
 public class SocketClientTest {
 
-    public static void main(String args[]){
+    public static void main(String args[]) {
         try {
             //1.建立客户端socket连接，指定服务器位置及端口
-            Socket socket =new Socket("localhost",8800);
+            Socket socket = new Socket("localhost", 8800);
             //2.得到socket读写流
-            OutputStream os=socket.getOutputStream();
-            PrintWriter pw=new PrintWriter(os);
+            OutputStream os = socket.getOutputStream();
+            PrintWriter pw = new PrintWriter(os);
             //输入流
-            InputStream is=socket.getInputStream();
-            BufferedReader br=new BufferedReader(new InputStreamReader(is));
+            InputStream is = socket.getInputStream();
+            BufferedReader br = new BufferedReader(new InputStreamReader(is));
             //3.利用流按照一定的操作，对socket进行读写操作
-            String info="用户名：Tom,用户密码：123456";
+            String info = "用户名：Tom,用户密码：123456";
             pw.write(info);
             pw.flush();
             socket.shutdownOutput();
             //接收服务器的相应
-            String reply=null;
-            while(!((reply=br.readLine())==null)){
-                System.out.println("接收服务器的信息："+reply);
+            String reply = null;
+            while (!((reply = br.readLine()) == null)) {
+                System.out.println("接收服务器的信息：" + reply);
             }
             //4.关闭资源
             br.close();
--- src/test/java/com/socket/test/socketdemo/tcp/SocketServerTest.java
+++ src/test/java/com/socket/test/socketdemo/tcp/SocketServerTest.java
@@ -1,4 +1,4 @@
-package com.socket.test.socketdemo;
+package com.socket.test.socketdemo.tcp;
 
 import java.io.*;
 import java.net.ServerSocket;
@@ -12,22 +12,22 @@ public class SocketServerTest {
     public static void main(String[] args) {
         try {
             //1.建立一个服务器Socket(ServerSocket)绑定指定端口
-            ServerSocket serverSocket=new ServerSocket(8800);
+            ServerSocket serverSocket = new ServerSocket(8800);
             //2.使用accept()方法阻止等待监听，获得新连接
-            Socket socket=serverSocket.accept();
+            Socket socket = serverSocket.accept();
             //3.获得输入流
-            InputStream is=socket.getInputStream();
-            BufferedReader br=new BufferedReader(new InputStreamReader(is));
+            InputStream is = socket.getInputStream();
+            BufferedReader br = new BufferedReader(new InputStreamReader(is));
             //获得输出流
-            OutputStream os=socket.getOutputStream();
-            PrintWriter pw=new PrintWriter(os);
+            OutputStream os = socket.getOutputStream();
+            PrintWriter pw = new PrintWriter(os);
             //4.读取用户输入信息
-            String info=null;
-            while(!((info=br.readLine())==null)){
-                System.out.println("我是服务器，用户信息为："+info);
+            String info = null;
+            while (!((info = br.readLine()) == null)) {
+                System.out.println("我是服务器，用户信息为：" + info);
             }
             //给客户一个响应
-            String reply="welcome";
+            String reply = "welcome";
             pw.write(reply);
             pw.flush();
             //5.关闭资源
